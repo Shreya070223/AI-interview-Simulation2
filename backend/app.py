3309533820da4c6d4bb46d60f6e102f9e1e45944
Shreya070223 <ankook0987@gmail.com>
2026-06-13 20:43:53 +0530

    added summerization fn

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from pydantic import BaseModel
 from google import genai
 from transformers import pipeline
 from pypdf import PdfReader
 from dotenv import load_dotenv
 import os
+import transformers
 
 load_dotenv()
 
@@ -13,10 +14,10 @@ app=FastAPI()
 gemini=genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 
 summarizer=pipeline("summarization",model="facebook/bart-large-cnn")
-skill_extract=pipeline(
-    "token-classification",
-    model="dslim/bert-base-NER"
-)
+# skill_extract=pipeline(
+#     "token-classification",
+#     model="dslim/bert-base-NER"
+# )
 
 skills_db = [
     "Python",
@@ -28,15 +29,30 @@ skills_db = [
     ...
 ]
 
+def chunk_text(text, chunk_size=800):
+    return [text[i:i+chunk_size] for i in range(0, len(text), chunk_size)]
+
+def Summar(text):
+  chunks = chunk_text(text)
+  summaries = []
+  for chunk in chunks:
+    result = summarizer(chunk, max_length=150, min_length=50)
+    summaries.append(result[0]["summary_text"])
+  final_summary = " ".join(summaries)
+  return final_summary
+
+
+
 def generatingResumeInfo(text):
+  summary=Summar(text)
   text+='''   Extract project names and technologies from this resume text and
        Return JSON.'''
-  projects= gemini.model.generte_content(
-    model="gemini-2.5-flash",
-    contents=text
-  )
+  # projects= gemini.model.generte_content(
+  #   model="gemini-2.5-flash",
+  #   contents=text
+  # )
+  projects={"project1":"cropcare"}
   skills='abc'
-  summary='abd'
   return {
     "skills":skills,
     "summary":summary,
@@ -45,18 +61,18 @@ def generatingResumeInfo(text):
   
 
 @app.post("/uploadResume")
-def upload(pdf):
-  reader=PdfReader(pdf)
+def upload(file: UploadFile = File(...)):
+  reader=PdfReader(file.file)
   text = ""
   for page in reader.pages:
     text += page.extract_text()
   res=generatingResumeInfo(text)
   return {
-    "summary":res.summary,
-    "skills":res.skills,
-    "projects":res.projects
+    "summary": res["summary"],
+    "skills": res["skills"],
+    "projects": res["projects"]
   }
-  
+
   
 
 
